878f27194ee6c5137a22198186e588f52e69b5ec
jiaowagn <wangjiaohere@gmail.com>
2018-03-09 19:24:56 +0800

    change root when user signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :notes 
+  resources :notes
+
+  authenticated :user do
+    root 'notes#index', as: 'authenticated_root'
+  end
 
   root 'welcome#index'
 
