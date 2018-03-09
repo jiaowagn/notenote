cdea63b808cf9c6ecea3bc3bf57af615f3208669
jiaowagn <wangjiaohere@gmail.com>
2018-03-09 19:18:11 +0800

    add user_id to note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,21 +1,22 @@
 class NotesController < ApplicationController
   before_action :find_note, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   def index
-    @notes = Note.all.order("created_at DESC")
+    @notes = current_user.notes.order("created_at DESC")
   end
 
   def show
   end
 
   def new
-    @note = Note.new
+    @note = current_user.notes.build
   end
 
   def edit
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
     if @note.save
       redirect_to note_path(@note)
     else
